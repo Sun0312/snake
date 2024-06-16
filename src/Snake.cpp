97b09ba355148c047b72ecbcdd453abacf0b7a5b
Josh <joshuahanlim777@gmail.com>
2024-06-16 13:25:53 +0900

    looks like it snakeBody problem is solved...? not sure

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -11,9 +11,11 @@ SnakeBody::SnakeBody(int r, int c):Objects(r,c,symbol){
 
 Snake::Snake(int r, int c, char dir){
     this->dir = dir;
-    snakes.emplace_back(SnakeBody(r,c)); //칸이 줄보다 작아서 2배수or3배수될듯...
-    snakes.emplace_back(SnakeBody(r,c+1));
+    snakes.emplace_back(SnakeBody(r, c+1)); //칸이 줄보다 작아서 2배수or3배수될듯...
+    snakes.emplace_back(SnakeBody(r,c));
     snakes.emplace_front(SnakeBody(r,c+2));  //헤드
+
+    // for debugging
    int t = 0;
    for (auto it = snakes.begin(); it != snakes.end(); ++it) {
         mvprintw(0+t, 100, "snake init snakeBody at %d: %c", it->pos.col, it->symbol);
